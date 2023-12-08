7fbeb77c2be6f6903fadc3a05a2d4a5a8d209f04
AnasKhan1506 <akanaskhan1506@gmail.com>
2023-12-08 05:37:34 -0800

    Login and Reg

--- lib/loginview.dart
+++ lib/loginview.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaseauth/home.dart';
+import 'package:firebaseauth/register.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,15 +11,43 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  loginUser() async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => Homeview()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        TextField(),
-        TextField(),
-        ElevatedButton(onPressed: () {}, child: Text("Login")),
+        TextField(controller: emailController),
+        TextField(controller: passwordController),
+        ElevatedButton(
+            onPressed: () {
+              loginUser();
+            },
+            child: Text("Login")),
+        InkWell(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Register()));
+          },
+          child: Text("Already have an account?"),
+        )
       ],
     ));
   }
